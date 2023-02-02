d2a01a52c638efb0ed15a368a68d5065a505fdf8
Damien Rivet <damien.rivet@gmail.com>
2023-02-02 14:26:33 +0100

    Add project template

---
+++ OCTEST/OCTEST/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OCTEST
+//
+//  Created by Damien Rivet on 02/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
